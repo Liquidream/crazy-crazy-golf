f1f4fee69c5d461f1148bedcb6266b63e7a4606e
Paul <liquidream@hotmail.co.uk>
2019-07-12 07:53:30 +0100

    Starting off with Nikki's circles demo

--- main.lua
+++ main.lua
@@ -1,18 +1,87 @@
--- Welcome to your new Castle project!
--- https://castle.games/get-started
--- Castle uses Love2D 11.1 for rendering and input: https://love2d.org/
--- See here for some useful Love2D documentation:
--- https://love2d-community.github.io/love-api/
+local ui = castle.ui
 
-local total_time_elapsed = 0
+local circles = {}
+
+
+-- All the UI-related code is just in this function. Everything below it is normal game code!
+
+function castle.uiupdate()
+    -- Button for adding circles
+    if ui.button('add circle') then
+        table.insert(circles, {
+            x = love.graphics.getWidth() * math.random(),
+            y = love.graphics.getHeight() * math.random(),
+            radius = math.random(30, 105),
+            color = ({ 'red', 'blue', 'green' })[math.floor(3 * math.random()) + 1]
+        })
+    end
+
+    -- Section per circle
+    for i, circle in ipairs(circles) do
+        ui.section('circle ' .. i, function()
+            -- Labels of inputs (such as 'color', 'x', etc.) need to be unique only within each section
+            circle.color = ui.radioButtonGroup('color', circle.color, { 'red', 'blue', 'green' })
+            circle.x = ui.slider('x', circle.x, 0, love.graphics.getWidth())
+            circle.y = ui.slider('y', circle.y, 0, love.graphics.getHeight())
+            circle.radius = ui.slider('radius', circle.radius, 30, 105)
+        end)
+    end
+end
+
+
+-- Normal game code from here on!
+
+local labelFont = love.graphics.newFont(28)
+
+love.graphics.setBackgroundColor(0.494, 0.388, 0.494)
 
 function love.draw()
-  local y_offset = 8 * math.sin(total_time_elapsed * 3)
-  love.graphics.print('Edit main.lua to get started!', 400, 300 + y_offset)
-  love.graphics.print('Press Cmd/Ctrl + R to reload.', 400, 316 + y_offset)
+    love.graphics.push('all')
+    for i, circle in ipairs(circles) do
+        -- Draw the circle
+        if circle.color == 'red' then
+            love.graphics.setColor(1, 0.443, 0.443)
+        elseif circle.color == 'green' then
+            love.graphics.setColor(0.612, 1, 0.612)
+        elseif circle.color == 'blue' then
+            love.graphics.setColor(0.29, 0.643, 1)
+        end
+        love.graphics.circle('fill', circle.x, circle.y, circle.radius)
+
+        -- Draw a label over the circles to show which one is which
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.setFont(labelFont)
+        local label = tostring(i)
+        local tw, th = labelFont:getWidth(i), labelFont:getHeight()
+        love.graphics.print(label, circle.x - 0.5 * tw, circle.y - 0.5 * th)
+    end
+    love.graphics.pop()
+
+    love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
 end
 
-function love.update(dt)
-  total_time_elapsed = total_time_elapsed + dt
+
+-- To allow dragging circles with the mouse
+
+local draggingCircle
+
+function love.mousepressed(x, y)
+    for i = #circles, 1, -1 do
+        local circle = circles[i]
+        local distX, distY = circle.x - x, circle.y - y
+        if distX * distX + distY * distY <= circle.radius * circle.radius then
+            draggingCircle = circle
+            break
+        end
+    end
 end
 
+function love.mousereleased()
+    draggingCircle = nil
+end
+
+function love.mousemoved(x, y, dx, dy)
+    if draggingCircle then
+        draggingCircle.x, draggingCircle.y = draggingCircle.x + dx, draggingCircle.y + dy
+    end
+end
