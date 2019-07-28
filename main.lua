2eb66fd06c501babc5c9526c95b8391dd8b619d4
Paul <liquidream@hotmail.co.uk>
2019-07-28 17:20:31 +0100

    Start of course editor (paint program!)

--- main.lua
+++ main.lua
@@ -1,17 +1,31 @@
 require("sugarcoat/sugarcoat")
 sugar.utility.using_package(sugar.S, true)
+require("common")
+require("ui_input")
 
-local x = 64
-local y = 64
+
+
+-- UI-bound/global vars
+brushSize = 10
+
+-- local vars
 local mx = 0
 local my = 0
+local courseCanvas
 
 
 function love.load()
-  init_sugar("Hello world!", 128, 128, 3)
+  init_sugar("Hello world!", GAME_WIDTH, GAME_HEIGHT, GAME_SCALE)
   
+  use_palette(ak54)
+  screen_render_stretch(false)
+  screen_render_integer_scale(false)
   set_frame_waiting(60)
   
+  -- course gfx setup
+  courseCanvas = new_surface(GAME_WIDTH, GAME_HEIGHT, "courseCanvas")
+
+  -- control setup
   player_assign_ctrlr(0, 0)
 
   register_btn(0, 0, input_id("keyboard", "left"))
@@ -23,21 +37,35 @@ function love.load()
   register_btn(6, 0, input_id("mouse_position", "x"))
   register_btn(7, 0, input_id("mouse_position", "y"))
   register_btn(8, 0, input_id("mouse_button", "lb"))
+
 end
 
 function love.update()
-  if btn(0) then x = x - 2 end
-  if btn(1) then x = x + 2 end
-  if btn(2) then y = y - 2 end
-  if btn(3) then y = y + 2 end
+  -- if btn(0) then x = x - 2 end
+  -- if btn(1) then x = x + 2 end
+  -- if btn(2) then y = y - 2 end
+  -- if btn(3) then y = y + 2 end
 
-  if btnv(6) then
+  -- mouse moved?
+  if btnv(6) or btnv(7) then
     mx, my = btnv(6), btnv(7)
+  end
+  -- mouse clicked? paint at current position
+  if btnv(8) > 0 then
+    -- switch to "paint" canvas
+    target("courseCanvas")
+    -- draw 
+    circfill(mx, my, brushSize, 8)
+    -- reset to "screen" again
+    target()
   end
 end
 
 function love.draw()
   cls()
-  circfill(mx, my, 4 + 2 * cos(t()), 3)
-  --circfill(x, y, 4 + 2 * cos(t()), 3)
+  -- draw current course data
+  spr_sheet("courseCanvas", 0, 0)
+  -- draw "cursor"
+  circfill(mx, my, brushSize, 8)
+  
 end
---
+++ ui_input.lua
@@ -0,0 +1,68 @@
+
+local ui = castle.ui
+
+
+-- All the UI-related code is just in this function. Everything below it is normal game code!
+
+function castle.uiupdate()
+
+    --ui.markdown('![](assets/title-text.png)')   
+
+    ui.markdown([[
+## CrAzY CrAzY Golf
+The craziest crazy golf! 🤪
+]])
+
+
+if ui.button('Save Course') then
+    -- TODO: save course to Castle storage
+end
+
+if ui.button('Load Course') then
+    -- TODO: load course to Castle storage
+end
+
+if ui.button('Reset Course') then
+    -- TODO: reset current course data
+end
+
+-- ui.section("Controls", function()
+
+--     ui.markdown([[
+-- ### Player Controls
+-- **⬆⬇⬅➡** = *Turn*
+
+-- **\[SPACE\]** = *Boost!*
+
+-- ### Advanced controls
+-- **S** = *Toggle GFX Shader*
+-- ]])
+
+-- end)
+
+
+--     ui.markdown([[
+-- #### Other Settings
+-- ]])
+
+--     ui.section("Shader settings", function()
+
+--         ui.toggle("Shader OFF", "Shader ON", useShader,
+--         { onToggle = function()
+--             useShader = not useShader
+--             shader_switch(useShader)
+--         end }
+--         )
+
+--         local refresh = false
+--         shader_crt_curve      = ui.slider("CRT Curve",      shader_crt_curve,      0, 0.25, { step = 0.0025, onChange = function() refresh = true end })
+--         shader_glow_strength  = ui.slider("Glow Strength",  shader_glow_strength,  0, 1,    { step = 0.01, onChange = function() refresh = true end })
+--         shader_distortion_ray = ui.slider("Distortion Ray", shader_distortion_ray, 0, 10,   { step = 0.1, onChange = function() refresh = true end })
+--         shader_scan_lines     = ui.slider("Scan Lines",     shader_scan_lines,     0, 1.0,  { step = 0.01, onChange = function() refresh = true end })
+--         if refresh then update_shader_parameters() end
+
+--     end)
+
+
+
+end
